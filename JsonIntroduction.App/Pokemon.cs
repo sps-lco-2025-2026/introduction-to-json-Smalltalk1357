f124d8557058bbec8622e6368424ce38ac72d0ee
Smalltalk1357 <alex.wan@icloud.com>
2026-04-21 21:02:02 +0100

    Added fuzzy search, added ability to see moves.

--- JsonIntroduction.App/Pokemon.cs
+++ JsonIntroduction.App/Pokemon.cs
@@ -1,5 +1,6 @@
 using System.Net.ServerSentEvents;
 using Newtonsoft.Json.Linq;
+using FuzzySharp;
 
 namespace JsonIntroduction.App
 {
@@ -7,18 +8,24 @@ namespace JsonIntroduction.App
     {
         public static void Run()
         {
-            (int index, bool random) = GetIndex();
+            bool repeat = true;
+            int index = 25;
+            while (repeat)
+                (index, repeat) = GetIndex();
+            
             JObject jsonObject = GetJsonObject($"pokemon/{index}");
             
             Console.Clear();
             
+            // Basic information
             string name = Capitalise(jsonObject["name"]!.ToString());
             Console.WriteLine($"---------- Pokemon #{index}: {name} ----------");
             Console.WriteLine($"Height: {jsonObject["height"]}");
             Console.WriteLine($"Weight: {jsonObject["weight"]}");
             Console.WriteLine($"Type: {string.Join(", ", UnpackTypes(jsonObject["types"]))}");
             
-            Console.WriteLine("\nMain Stats:");
+            // Main stats
+            Console.WriteLine("\nBase Stats:");
             List<int> stats = UnpackStats(jsonObject["stats"]);
             string[] statName = ["HP", "Attack", "Defense", "Special Attack", "Special Defense", "Speed"];
             for (int i = 0; i < stats.Count; i++)
@@ -31,16 +38,25 @@ namespace JsonIntroduction.App
             int total = stats.Sum();
             Console.WriteLine($"{"Total", 15}|{string.Concat(Enumerable.Repeat("▋", 30 * total / 800)), -30}|{total}");
             
+            // Abilities
             Console.WriteLine("\nAbilities:");
             List<Tuple<string, string>> abilities = UnpackAbilities(jsonObject["abilities"]);
             for (int i = 0; i < abilities.Count; i++)
             {
                 Console.WriteLine($"{i+1}: {abilities[i].Item1} (Slots: {abilities[i].Item2})");
             }
-
-            if (random)
+            
+            // Moves
+            Console.Write("\nView moves? (y/n): ");
+            if (Console.ReadLine() == "y")
             {
-                Console.WriteLine("\nRandomly selected Pokemon. If not correct pokemon, check spelling/ID");
+                List<(string, int)> moves = UnpackMoves(jsonObject["moves"]);
+                Console.WriteLine($"\nMoves (Based off Pokemon Red-Blue): (Count: {moves.Count})");
+
+                for (int i = 0; i < moves.Count; i++)
+                {
+                    Console.WriteLine($"{i + 1,3}: {moves[i].Item1,-20} (Learnt at level {moves[i].Item2})");
+                }
             }
         }
 
@@ -55,7 +71,8 @@ namespace JsonIntroduction.App
 
             return o;
         }
-
+        
+        // returns index of pokemon and whether it needs to be repeated (instead of recursion)
         private static (int, bool) GetIndex()
         {
             JObject o = GetJsonObject("pokemon-species?limit=10000&offset=0");
@@ -70,21 +87,54 @@ namespace JsonIntroduction.App
 
             Console.WriteLine($"Current total number of pokemon: {count}");
             Console.Write("Enter pokemon name or ID (enter for random): ");
-            
+
             string input = Console.ReadLine()!.ToLower();
-            if (int.TryParse(input, out int index))
+            
+            Random r = new();
+            int rNum = r.Next(0, count) + 1;
+            
+            if (input == "") // Random
+            {
+                return (rNum, false);
+            }
+            
+            if (int.TryParse(input, out int index)) // Search by ID
             {
                 if (index > 0 && index <= count)
                     return (index, false);
             }
-            if (species.Contains(input))
+            
+            if (species.Contains(input)) // Search by name
             {
                 index = species.IndexOf(input) + 1;
                 return (index, false);
             }
-            Random r = new();
-            index = r.Next(0, count) + 1;
-            return (index, false);
+
+            // Fuzzy search by name - uses Levenshtein distance (via FuzzySharp)
+            List<(string, int)> results = [];
+            foreach (string name in species)
+            {
+                int score = Fuzz.PartialRatio(input, name);
+                if (score > 70) 
+                    results.Add((name, score));
+            }
+            results.Sort((n, s) => s.Item2.CompareTo(n.Item2));
+
+            if (results.Count == 0)
+            {
+                Console.WriteLine("Not matches found - try again.");
+                return (0, true);
+            }
+            
+            int num = results.Count < 5 ? results.Count : 5;
+            Console.WriteLine("Not found. Similar matches:");
+            for (int i = 0; i < num; i++)
+            {
+                Console.WriteLine($"{Capitalise(results[i].Item1)}");
+            }
+            Console.WriteLine();
+            
+            return (0, true);
         }
         
         // To unpack the lists of a Pokémon's stats requires knowledge of how the JSON is structured
@@ -105,7 +155,7 @@ namespace JsonIntroduction.App
 
         private static List<int> UnpackStats(JToken? lists)
         {
-            List<int> output = new();
+            List<int> output = [];
             
             foreach (JToken temp in lists!)
             {
@@ -118,7 +168,7 @@ namespace JsonIntroduction.App
 
         private static List<string> UnpackTypes(JToken? lists)
         {
-            List<string> output = new();
+            List<string> output = [];
 
             foreach (JToken temp in lists!)
             {
@@ -126,6 +176,20 @@ namespace JsonIntroduction.App
                 output.Add(Capitalise(type));
             }
 
+            return output;
+        }
+
+        private static List<(string, int)> UnpackMoves(JToken? lists)
+        {
+            List<(string, int)> output = [];
+            
+            foreach (JToken temp in lists!)
+            {
+                string move = temp["move"]!["name"]!.ToString();
+                int levelLearnt = (int)temp["version_group_details"]![0]!["level_learned_at"]!;
+                output.Add((Capitalise(move), levelLearnt));
+            }
+            
             return output;
         }
         
